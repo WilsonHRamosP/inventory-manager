e3f79ba99dd3d23a68385007a45cf11ceffb8a98
wilson Ramos <wisitom@hotmail.com>
2026-08-06 17:03:26 -0500

    Inicial estructura del proyecto

---
+++ src/test/java/com/wildev/inventor/InventoryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildev.inventor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
